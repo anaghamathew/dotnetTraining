df015fdbeee5e95e00b33f732f0c9f5429455021
Anagha Mathew <anagha.mathew@foundingminds.com>
2021-02-05 16:49:00 +0530

    initial commit forrestaurent app

---
+++ RestaurentApp/Controllers/MenuCategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurentApp.Controllers
+{
+    public class MenuCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RestaurentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestaurentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
